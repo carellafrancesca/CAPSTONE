f662d6359113bb9e1e6d69ee27b820c61c38a175
Francesca <carellafrancesca29@gmail.com>
2023-09-08 16:21:34 +0200

    update

--- FRONT-END/capstone/src/environment/environment.development.ts
+++ FRONT-END/capstone/src/environment/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  register: "http://localhost:8080/ees/auth/register",
-  login: "http://localhost:8080/ees/auth/login"
+  register: "http://localhost:8080/api/auth/register",
+  login: "http://localhost:8080/api/auth/login"
 };
