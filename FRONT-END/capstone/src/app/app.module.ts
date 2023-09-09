e153d619701c282caeb74c779214190db1e1f305
Francesca <carellafrancesca29@gmail.com>
2023-09-09 15:47:53 +0200

    login e register scss

--- FRONT-END/capstone/src/app/app.module.ts
+++ FRONT-END/capstone/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -11,7 +10,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     LoginComponent,
     RegisterComponent
   ],
