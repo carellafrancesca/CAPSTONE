0275a28589fb856f64115c8eec7ed7f3d6be9d0a
Francesca <carellafrancesca29@gmail.com>
2023-09-15 19:39:12 +0200

    update

--- FRONT-END/capstone/src/app/pages/homepage/singleconcert/singleconcert.component.ts
+++ FRONT-END/capstone/src/app/pages/homepage/singleconcert/singleconcert.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-singleconcert',
   templateUrl: './singleconcert.component.html',
   styleUrls: ['./singleconcert.component.scss']
 })
-export class SingleconcertComponent {
+export class SingleconcertComponent implements OnInit{
+
+  username: string = '';
+
+  constructor(
+    private authSvc: AuthService,
+    private userSvc: UserService,
+    private router: Router,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+    this.username = this.userSvc.getUsername();
+  }
+
+  logout(){
+    this.authSvc.logout()
+  }
+
 
 }
